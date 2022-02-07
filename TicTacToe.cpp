5cfca8e3e3f17bf4094ee371a142c51d3573566f
Bella Barbieri <59892760+bellab8201@users.noreply.github.com>
2022-02-07 12:30:40 -0700

    Created TicTacToe.cpp with CreateBoard feature

---
+++ TicTacToe.cpp
@@ -0,0 +1,25 @@
+// create a 3x3 matrix and print it out
+#include <iostream>
+void CreateBoard(int board[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            board[i][j] = 0;
+        }
+    }
+}
+int main()
+{
+    int board[3][3];
+    CreateBoard(board);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout << board[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
